be8ef60491f69acce2cbb3d4d145968c283cd552
alinegrazii <abner.lborges@hotmail.com>
2023-02-16 19:51:03 -0300

    go

--- upload.php
+++ upload.php
@@ -1,7 +1,25 @@
 <?php
 
-if(isset($_POST["enviar"])){
-  echo "Funcionando";
+$_UP['folder'] = './enviados';
+$_UP['weigth'] = 1024 * 1024 * 2;
+$_UP['extensions'] = array('jpg','png','jpeg','gif');
+$_UP['rename'] = false;
+
+$_UP['erros'][0] = 'Não houve erro';
+$_UP['erros'][1] = 'O arquivo no upload é maior do que o limite do PHP';
+$_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
+$_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
+$_UP['erros'][4] = 'Não foi feito o upload do arquivo';
+
+if($_FILES['arquivo']['error'] != 0){
+  die("Não foi possível fazer o Upload, erro: " . $_UP['erros'][$_FILES['arquivo']['error']]);
+  exit
 }
+
+$extension = strtolower(end(explode('.', $_FILES['arquivo']['name'])));
+  if(array_search($extension, $_UP['extensions']) === false);{
+    echo "Por favor envie arquivos do tipo: jpg, png, jpeg ou gif";
+  }
+
 
 ?>
